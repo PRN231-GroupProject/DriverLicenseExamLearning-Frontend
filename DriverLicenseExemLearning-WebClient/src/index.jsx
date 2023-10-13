95e58dd267161fc5182e8b64b95fa560a82a9a85
dangphan15 <minhdang12345c@gmail.com>
2023-10-13 16:39:43 +0700

    init project

---
+++ DriverLicenseExemLearning-WebClient/src/index.jsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from "react-router-dom";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+);
